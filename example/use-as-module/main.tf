8143315cc23d06ee3cbeb95604a40f3d1070e5c0
lfeldman <lukasz.feldman@oracle.com>
2021-11-08 14:32:13 +0100

    First commit

---
+++ example/use-as-module/main.tf
@@ -0,0 +1,50 @@
+## Copyright (c) 2021, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+variable "region" {}
+variable "fingerprint" {}
+variable "user_ocid" {}
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "private_key_path" {}
+variable "availability_domain_name" {}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
+module "hub-spoke" {
+  #source                           = "github.com/oracle-devrel/terraform-oci-arch-hub-spoke"
+  source                           = "../../"
+  tenancy_ocid                     = var.tenancy_ocid
+  user_ocid                        = var.user_ocid
+  fingerprint                      = var.fingerprint
+  region                           = var.region
+  private_key_path                 = var.private_key_path
+  compartment_ocid                 = var.compartment_ocid
+  availability_domain_name         = var.availability_domain_name
+  deploy_bastion_instance          = false # no bastion host in HUB VCN
+  hub_vcn_cidr_block               = "192.168.0.0/16"
+  hub_subnet_pub01_cidr_block      = "192.168.1.0/24"
+  deploy_spoke01_instance          = false # no machine in spoke01 VCN
+  spoke01_vcn_cidr_block           = "10.10.0.0/16"
+  spoke01_subnet_priv01_cidr_block = "10.10.0.0/24"
+  deploy_spoke02_instance          = false # no machine in spoke02 VCN
+  spoke02_vcn_cidr_block           = "172.16.0.0/16"
+  spoke02_subnet_priv01_cidr_block = "172.16.1.0/24"
+}
+
+output "vcn_id_hub_id" {
+  value = module.hub-spoke.vcn_id_hub_id
+}
+output "vcn_id_spoke01_id" {
+  value = module.hub-spoke.vcn_id_spoke01_id
+}
+
+output "vcn_id_spoke02_id" {
+  value = module.hub-spoke.vcn_id_spoke02_id
+}
---
+++ hub.tf
@@ -0,0 +1,66 @@
+## Copyright (c) 2021, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+resource "oci_core_vcn" "hub" {
+  cidr_block     = var.hub_vcn_cidr_block
+  dns_label      = var.hub_vcn_dns_label
+  compartment_id = var.compartment_ocid
+  display_name   = var.hub_vcn_display_name
+  defined_tags   = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+}
+
+#IGW
+resource "oci_core_internet_gateway" "hub_internet_gateway" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.hub.id
+  enabled        = "true"
+  display_name   = "IGW_HUB"
+  defined_tags   = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+}
+
+#Default route table hub
+resource "oci_core_default_route_table" "hub_default_route_table" {
+  manage_default_resource_id = oci_core_vcn.hub.default_route_table_id
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.hub_internet_gateway.id
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+  route_rules {
+    network_entity_id = oci_core_local_peering_gateway.hub_spoke01_local_peering_gateway.id
+    destination       = var.spoke01_vcn_cidr_block
+    destination_type  = "CIDR_BLOCK"
+  }
+  route_rules {
+    network_entity_id = oci_core_local_peering_gateway.hub_spoke02_local_peering_gateway.id
+    destination       = var.spoke02_vcn_cidr_block
+    destination_type  = "CIDR_BLOCK"
+  }
+  defined_tags = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+}
+
+# Peering connections to the spokes
+resource "oci_core_local_peering_gateway" "hub_spoke01_local_peering_gateway" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.hub.id
+  display_name   = "hub_spoke01"
+  peer_id        = oci_core_local_peering_gateway.spoke01_hub_local_peering_gateway.id
+  defined_tags   = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+}
+
+resource "oci_core_local_peering_gateway" "hub_spoke02_local_peering_gateway" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.hub.id
+  display_name   = "hub_spoke02"
+  peer_id        = oci_core_local_peering_gateway.spoke02_hub_local_peering_gateway.id
+  defined_tags   = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+}
+#Hub pub subnet
+resource "oci_core_subnet" "hub_subnet_pub01" {
+  cidr_block     = var.hub_subnet_pub01_cidr_block
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.hub.id
+  display_name   = var.hub_subnet_pub01_display_name
+  defined_tags   = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+}
+
